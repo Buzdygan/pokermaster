773be986e32cc73e07876a503773aca8bf5242fc
Jakub Tlałka <mamba57@gmail.com>
2013-12-19 17:28:49 +0100

    df

--- src/Utils.h
+++ src/Utils.h
@@ -8,15 +8,15 @@ const int DEFAULT_INITIAL_CASH = 100;
 const int DEFAULT_ROUNDS_NUMBER = 1;
 const int SMALL_BLIND = 4;
 const int BIG_BLIND = 8;
-const int CARDS_NUMBER = 52;
-const int LAST_PHASE_NUMBER = 3;
+const int CARDS_NUMBER = 6;
+const int LAST_PHASE_NUMBER = 1;
 const int DRAW = -1;
 const int GAME_DRAWN = 6;
-const int MAX_GAME_CARDS = 9;
+const int MAX_GAME_CARDS = 3;
 
 const int GAME_IN_PROGRESS = 5;
 
-const int PHASE_TABLE_CARDS[2][4][2] = {{{0,2}, {4,7}, {7,8}, {8,9}}, {{2,4}, {4,7}, {7,8}, {8,9}}};
+const int PHASE_TABLE_CARDS[2][2][2] = {{{0,1}, {2,3}}, {{1,2}, {2,3}}};
 
 int evaluateCards(vector<int> cards);
 
